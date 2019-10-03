7bdf9c44c5974c1cac2fd218f80b6e2db1d316f9
unknown <cole.spinali@yahoo.com>
2019-10-03 10:12:07 -0700

    First commit

--- Sub.v
+++ Sub.v
@@ -1,26 +1,22 @@
 `timescale 1ns / 1ps
 //Test Subtractor
-module testSub(a_1,b_1,clk,diff_1);
-	input [1:0] a_1;
-	input [1:0] b_1;
-	input clk;
-	output [1:0] diff_1;
+module testSub(a_1,b_1,diff_1);
+	input [31:0] a_1;
+	input [31:0] b_1;
+	output [31:0] diff_1;
 	
 	
-	Sub #(.WIDTH(2)) Sub_1(a_1,b_1,clk,diff_1);
+	Sub #(.WIDTH(32)) Sub_1(a_1,b_1,diff_1);
 	endmodule
 //Subtractor Module
-module Sub #(parameter WIDTH = 2) (a,b,clk,diff);
+module Sub #(parameter WIDTH = 32) (a,b,diff);
 
 input [WIDTH - 1: 0] a;
 input [WIDTH - 1: 0] b;
-input clk;
-output reg [WIDTH - 1: 0] diff;
+output [WIDTH - 1: 0] diff;
 
 
-	always @ (posedge clk) begin
-	begin
-			diff <= a - b;
-	end
-end
+	
+	assign{diff} = a - b;
+	
 endmodule //End Subtractor Module
